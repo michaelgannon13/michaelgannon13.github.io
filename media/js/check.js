164732b68d80772ea8f933621567a97904c5c7a2
Michael gannon <michael.gannon13@gmail.com>
2016-07-31 21:20:03 +0100

    modal working

--- media/js/check.js
+++ media/js/check.js
@@ -4,14 +4,19 @@ var units = "px";
 
 function getMediaQuery(width, height){
 
+
+
       width = width + units;
       height = height + units;
 
       var media = "@media only screen ";
-      var maxWidth = "and (max-width : " + width + ") "
-      var maxHeight = "and (max-height : " + height + ") {}" 
+      var maxWidth = "and (max-width : " + width + ") ";
+      var maxHeight = "and (max-height : " + height + ") " ;
+      var brack = "{}";
 
-      alert(media + maxWidth + maxHeight);
+      ///alert(media + maxWidth + maxHeight);
 
-      $('#myModal').modal();   
+      $('#gen-media').text(media + maxWidth + maxHeight + brack);
+
+      $('#myModal').modal();
   }
