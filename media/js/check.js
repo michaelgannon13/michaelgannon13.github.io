428c4d98711fa8db319ef15441a64f4b3e95d92e
Michael Gannon <michael.gannon13@gmail.com>
2016-08-02 14:50:35 +0100

    local fonts added

--- media/js/check.js
+++ media/js/check.js
@@ -3,19 +3,23 @@ var height;
 var units = "px";
 
 function getMediaQuery(width, height){
-      $("#copy-button").text("Copy")
+      $("#copy-button").text("Copy");
+      $("#copy-button").css("background-color", "#337ab7").css("border-color", "#337ab7").css("font-weight", "500");
 
       width = width + units;
       height = height + units;
 
       var media = "@media only screen ";
-      var maxWidth = "and (max-width : " + width + ") ";
-      var maxHeight = "and (max-height : " + height + ") " ;
-      var brack = "{}";
+      var maxWidth = "   " + "and (max-width : " + width + ") ";
+      var maxHeight = "   " + "and (max-height : " + height + ") " + "{" ;
+      var css = "   " + "/* CSS here */" ;
+      var brack = "\n" + "}";
+
+
 
       ///alert(media + maxWidth + maxHeight);
 
-      $('#gen-media').text(media + maxWidth + maxHeight + brack);
+      $('#gen-media').text(media + "\n" + maxWidth + "\n" + maxHeight + "\n" + css + brack);
 
       $('#myModal').modal();
   }
