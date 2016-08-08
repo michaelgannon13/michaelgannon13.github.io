9fb8159b058efe575ec141dc32ed41c4055b81bc
Michael Gannon <michael.gannon13@gmail.com>
2016-08-08 13:02:09 +0100

    ios and windows added

--- js/master-min.js
+++ js/master-min.js
@@ -46,60 +46,84 @@
               center: {lat: 53.3004978, lng: -6.2838544},
               zoom: 14,
               scrollwheel: false,
-              styles: [
-                {
+              styles: [{
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "invert_lightness": true
+            },
+            {
+                "saturation": 20
+            },
+            {
+                "lightness": 50
+            },
+            {
+                "gamma": 0.4
+            },
+            {
+                "hue": "#00ffee"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels",
         "stylers": [
             {
                 "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#ffffff"
             },
             {
-                "saturation": -100
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#405769"
             }
         ]
     },
     {
         "featureType": "water",
+        "elementType": "geometry.fill",
         "stylers": [
             {
-                "visibility": "on"
-            },
-            {
-                "saturation": 100
-            },
-            {
-                "hue": "#00ffe6"
+                "color": "#232f3a"
             }
         ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "saturation": 100
-            },
-            {
-                "hue": "#00ffcc"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    }
-              ]
+    }]
               
             });
 
