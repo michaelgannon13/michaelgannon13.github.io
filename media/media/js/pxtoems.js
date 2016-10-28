248e9fe693b632002b25ac101f78ca2b79516aa3
Michael Gannon <michael.gannon13@gmail.com>
2016-10-28 17:13:32 +0100

    ems

--- media/media/js/pxtoems.js
+++ media/media/js/pxtoems.js
@@ -9,19 +9,19 @@ function convert(){
 		var roundEms = emsInput.toFixed(2);
 
 		var perVal = emsInput*100;
-		var perValEms = perVal.toFixed(2);
+		var perValEms = perVal.toFixed(1);
 
 		ptInput = pxInput*0.75;
 		var roundPts = ptInput.toFixed(2);
 
 		document.getElementById("ems").value = roundEms;
-		document.getElementById("ems").style.color = "#fc5185";
+		document.getElementById("ems").style.color = "#47D0BD";
 
 		document.getElementById("pts").value = roundPts;
-		document.getElementById("pts").style.color = "#fc5185";
+		document.getElementById("pts").style.color = "#47D0BD";
 
 		document.getElementById("perc").value = perValEms;
-		document.getElementById("perc").style.color = "#fc5185";
+		document.getElementById("perc").style.color = "#47D0BD";
 	} else {
 		document.getElementById("ems").value = "Ems";
 		document.getElementById("ems").style.color = "#ccc";
