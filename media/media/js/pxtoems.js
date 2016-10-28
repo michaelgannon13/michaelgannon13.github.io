f1e1ae04297ad750f66e48787037468b21b77223
Michael Gannon <michael.gannon13@gmail.com>
2016-10-28 15:39:13 +0100

    ems

--- media/media/js/pxtoems.js
+++ media/media/js/pxtoems.js
@@ -1,14 +1,26 @@
 var emsInput = $('ems').val();
 
 function convert(){
-
+	var pxInput = document.getElementById("px").value;
+	var baseInput = document.getElementById("base").value;
 	if (pxInput !== "") {
-		var pxInput = document.getElementById("px").value;
-		var baseInput = document.getElementById("base").value;
+		
 		emsInput = pxInput/baseInput;
-		document.getElementById("ems").value = pxInput/baseInput;
+		var roundEms = emsInput.toFixed(2);
+		document.getElementById("ems").value = roundEms;
+		document.getElementById("ems").style.color = "#fc5185";
 	} else {
-		document.getElementById("ems").value = "";
+		document.getElementById("ems").value = "Ems";
+		document.getElementById("ems").style.color = "#ccc";
 	}
-	//setInterval(convert, 500);
+
+	var num = "987238";
+
+	if(num.match(/^\d+$/)){
+	  //valid integer
+	}else if(num.match(/^\d+\.\d+$/)){
+	  //valid float
+	}else{
+	  //not valid number
+	}
 }
