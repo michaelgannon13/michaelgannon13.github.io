566fcb81e250123073984368bfd26c60cb74ebc7
Michael Gannon <michael.gannon13@gmail.com>
2016-11-01 16:30:34 +0000

    changes

--- media/media/js/minify.js
+++ media/media/js/minify.js
@@ -1,16 +1,22 @@
+var cssMin = document.getElementById("cssmin").value;
+
 function minify(){
-	var css = document.getElementById("css").value;
-	var cssMin = document.getElementById("cssmin").value;
 
 	if (css !== "") {
+		var css = document.getElementById("css").value;
 		var minified = css.replace(/\s/g,'')
 
 		$("#cssmin").text(minified);
 
-		document.getElementById("perc").value = perValEms;
-		document.getElementById("perc").style.color = "#47D0BD";
+		// document.getElementById("perc").value = perValEms;
+		// document.getElementById("perc").style.color = "#47D0BD";
 	} else {
 		$("#css").text("Enter CSS here");
 		$("#cssmin").text("");
 	}
+}
+
+function clearText(){
+	document.getElementById("css").value = "";
+	$("#cssmin").text("");
 }
