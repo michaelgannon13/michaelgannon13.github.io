4bf49e183e71ff16fa811f69d8cef7bbdcb1a98a
Michael Gannon <michael.gannon13@gmail.com>
2016-11-01 17:04:17 +0000

    changes

--- media/media/js/minify.js
+++ media/media/js/minify.js
@@ -7,16 +7,20 @@ function minify(){
 		var minified = css.replace(/\s/g,'')
 
 		$("#cssmin").text(minified);
+		$("#copy-button2").click();
+		$("#notice").css("visibility", "visible");
 
 		// document.getElementById("perc").value = perValEms;
 		// document.getElementById("perc").style.color = "#47D0BD";
 	} else {
 		$("#css").text("Enter CSS here");
 		$("#cssmin").text("");
+		$("#notice").css("visibility", "hidden");
 	}
 }
 
 function clearText(){
 	document.getElementById("css").value = "";
 	$("#cssmin").text("");
+	$("#notice").css("visibility", "hidden");
 }
