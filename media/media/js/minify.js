79a0a91d452105e59a19fc0612fca3c19c2e110e
Michael Gannon <michael.gannon13@gmail.com>
2016-11-02 12:08:56 +0000

    sdaf

--- media/media/js/minify.js
+++ media/media/js/minify.js
@@ -7,18 +7,19 @@ function minify(){
 		var minified = css.replace(/\s/g,'')
 
 		$("#cssmin").text(minified);
-		//$("#copy-button2").click();
-		//$("#notice").css("visibility", "visible");
 
 	} else {
 		$("#css").text("Enter CSS here");
 		$("#cssmin").text("");
-		//$("#notice").css("visibility", "hidden");
 	}
 }
 
 function clearText(){
 	document.getElementById("css").value = "";
 	$("#cssmin").text("");
-	//$("#notice").css("visibility", "hidden");
+
+	 $("#copy-button2").text("Copy");
+      $("#copy-button2").css("background-color", "#E82597").css("border-color", "#E82597").css("font-weight", "900");
+      $("#ems").css("border-color", "#47D0BD");
+      $("#ems").css("color", "#47D0BD").css("border-width", "12px");
 }
