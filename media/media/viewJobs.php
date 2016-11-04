d7f96489b98995a2c1b0012b64f537f27922eef4
Michael gannon <michael.gannon13@gmail.com>
2016-11-04 12:58:41 +0000

    am

--- media/media/viewJobs.php
+++ media/media/viewJobs.php
@@ -1,12 +1,12 @@
 <!doctype html>
 <html lang="en-US" class="export">
 <head>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3919315193284770",
-    enable_page_level_ads: true
-  });
+<!-- <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+ --><script>
+  // (adsbygoogle = window.adsbygoogle || []).push({
+  //   google_ad_client: "ca-pub-3919315193284770",
+  //   enable_page_level_ads: true
+  // });
 </script>
 
   <meta charset="utf-8">
@@ -50,7 +50,13 @@
 <body id="main-container" class="page--index" data-page="index">
     <a id="bookmarkme" href="#" rel="sidebar" title="bookmark this page"><span id="b-text">Bookmark</span><img alt="simple-bookmark" id="bookmark" src="img/agenda.png"> </a>
 
-
+<style type="text/css">
+  div#share-buttons {
+    position: initial; 
+    bottom: 0;
+    width: 100%;
+}
+</style>
 
 <div id="pagina">
         <div class="barra-menu">
@@ -66,11 +72,149 @@
             </ul>
         </div>
     </div>
+    <script type="text/javascript">
+      function launchModal(){
+        $('#myModal').modal();
+      }
+    </script>
 
 <div class="hero">
   <div class="primary-content">
   <h1 id="title">
     <span id="simple">Simple CSS</span> Jobs</h1>
+    <p id="jobsforwho">Jobs for Web Designers, Front End Developers, Responsive Designers, Wordpress Developers, Angular Developers, User Experience Designers</p>
+    <button id="post" type="button" class="btn btn-info btn-lg" onclick="launchModal();" data-toggle="modal" >Post Job</button>
+
+    
+
+
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog modal-lg">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Post Job below</h4>
+          </div>
+          <div class="modal-body">
+            
+  <form action="process.php" method="post" class="form-horizontal" style="top:0px;">
+<fieldset>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="company"><i class="fa fa-briefcase" aria-hidden="true"></i>Company</label>  
+  <div class="col-md-4">
+  <input id="textinput" name="company" type="text" placeholder="Company..." class="form-control input-md" required="">
+  </div>
+</div>
+
+<!-- Select Basic -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="salary"><i class="fa fa-eur" aria-hidden="true"></i>Salary</label>
+  <div class="col-md-4">
+    <select id="selectbasic" name="salary" class="form-control">
+      <option value="Negotiable">Negotiable</option>
+      <option value="20,000-30,000">20,000 - 30,000</option>
+      <option value="20,000-30,000">20,000 - 30,000</option>
+      <option value="40,000-50,000">40,000 - 50,000</option>
+      <option value="Contact for details">Contact for details</option>
+    </select>
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="location"><i class="fa fa-map-marker" aria-hidden="true"></i>Location</label>  
+  <div class="col-md-4">
+  <input id="textinput" name="location" type="text" placeholder="Location..." class="form-control input-md" required="">
+  </div>
+</div>
+
+<!-- Multiple Radios (inline) -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="duration"><i class="fa fa-clock-o" aria-hidden="true"></i>Duration</label>
+  <div class="col-md-4"> 
+    <label class="radio-inline" for="radios-0">
+      <input type="radio" name="duration" id="radios-0" value="Permanent" checked="checked">
+      Permanent
+    </label> 
+    <label class="radio-inline" for="radios-1">
+      <input type="radio" name="duration" id="radios-1" value="Contract">
+      Contract
+    </label>
+  </div>
+</div>
+
+<!-- Select Basic -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="experience"><i class="fa fa-handshake-o" aria-hidden="true"></i>Experience</label>
+  <div class="col-md-4">
+    <select id="selectbasic" name="experience" class="form-control">
+      <option value="Internship">Internship</option>
+      <option value="1 year - 3 years">1 year - 3 years</option>
+      <option value="3 year - 5 years">3 years - 5 years</option>
+      <option value="5 year - 10 years">5 years - 10 years</option>
+    </select>
+  </div>
+</div>
+
+<!-- Select Multiple -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="skills"><i class="fa fa-code" aria-hidden="true"></i>Skills Required</label>
+  <div class="col-md-4">
+    <select id="selectmultiple" name="skills" class="form-control" multiple="multiple">
+      <option value="HTML">HTML</option>
+      <option value="CSS">CSS</option>
+      <option value="Bootstrap/Foundation">Bootstrap/Foundation</option>
+      <option value="Javascript">Javascript</option>
+      <option value="Angular.js">Angular.js</option>
+      <option value="Backbone.js">Backbone.js</option>
+      <option value="Ember.js">Ember.js</option>
+      <option value="Adobe Suite">Adobe Suite</option>
+    </select>
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="contact"><i class="fa fa-envelope-o" aria-hidden="true"></i>Contact</label>  
+  <div class="col-md-4">
+  <input id="textinput" name="contact" type="text" placeholder="Contact..." class="form-control input-md" required="">
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="url"><i class="fa fa-television" aria-hidden="true"></i>Link</label>  
+  <div class="col-md-4">
+  <input id="textinput" name="url" type="text" placeholder="Applciation" class="form-control input-md" required="">
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button id="singlebutton" type="submit" name="singlebutton" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+        
+
 
     <?php
   $server = mysql_connect("mysql2779int.cp.blacknight.com", "u1429287_michael", "S@3+Nmr1K[?h|Tu5"); 
@@ -111,7 +255,6 @@
 
   <?php
      while ($row = mysql_fetch_array($query)) {?>
-
          <tbody>
           <tr>
             <td>
@@ -140,9 +283,6 @@
             </td>
           </tr>
         </tbody>
-
-
-
     <?php  } ?>
 </table>
 
@@ -231,27 +371,8 @@
 
  $('body').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
 </script>
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us14.list-manage.com","uuid":"30eda457c7d2bec3542d6d966","lid":"67381fd6e4"}) })</script>
-<script>
-$(document).ready(function () {
-
-    $("#mob").prop("checked", true);
-    /* Toggle menú de móviles  */
-    $('#btn-drop').on('click', function (e) {
-        e.preventDefault();
-        $('#submenu').slideToggle(500);
-    }); // fin click
-    
-    /* Hacer visible el menú al agrandar */
-    $(window).resize(function () {
-        if (innerWidth >= 480) {
-            if ($('#submenu').css('display') == 'none') {
-                $('#submenu').removeAttr('style');
-            }
-        }
-    }); // fin resize
-});
-</script>
+<!-- <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us14.list-manage.com","uuid":"30eda457c7d2bec3542d6d966","lid":"67381fd6e4"}) })</script>
+ -->
 
 </body>
 </html>
