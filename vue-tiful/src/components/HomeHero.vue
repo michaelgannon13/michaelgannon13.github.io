317a390896a82eb7e9d23432b34fc9ad7f36c997
Michael <m.j.gannon@accenture.com>
2019-10-31 10:41:42 +0100

    added vue

---
+++ vue-tiful/src/components/HomeHero.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fluid fill-height class="home-hero">
+    <v-layout justify-center align-center column pa-5>
+      <div class="display-4 font-weight-black white--text text-xs-center">
+        FRONT-END
+      </div>
+      <div class="display-4 font-weight-black white--text text-xs-center mb-3">
+        CONSULTANT
+      </div>
+      <div class="display-1 font-weight-bold white--text text-xs-center">
+        Michael Gannon
+      </div>
+      <a href="#details" class="button">
+        <v-btn fab class="mt-5 blue darken-4">
+          <v-icon large color="white">expand_more</v-icon>
+        </v-btn>
+      </a>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomeHero"
+};
+</script>
+
+<style scoped>
+.home-hero {
+  background: url("./../assets/images/deveoper-stock.jpg");
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  max-height: 100vh;
+  filter: hue-rotate(50deg);
+}
+
+a.button {
+  text-decoration: none;
+}
+</style>
---
+++ vue-tiful/src/components/HomeSkills.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container id="skills" grid-list-lg>
+    <v-layout row>
+      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">What I can do for you</v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md4 v-for="(skill, idx) in skills" :key="idx">
+        <v-card>
+          <v-img :src="skill.image" height="500px">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">{{ skill.name }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ skill.name }}</h3>
+              <div class="card-body">
+                {{ skill.description }}
+              </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import Skills from '../assets/data/skills-section/skills.json'
+
+export default {
+  name: "HomeSkills",
+  data() {
+    return {
+      skills: Skills 
+    };
+  }
+};
+</script>
+
+<style scoped>
+.card-body {
+  font-weight: 300;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  min-height: 11rem;
+}
+</style>
